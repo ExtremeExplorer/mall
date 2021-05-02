17578323639c1a7f9e4bab216abc83ad40476bbc
Explorer <2320255405@qq.com>
2021-05-02 17:42:31 +0800

    optimized code

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -11,7 +11,9 @@
     </slot>
     <div class="indicator">
       <slot name="indicator" v-if="showIndicator && slideCount>1">
-        <div v-for="(item, index) in slideCount" class="indi-item" :class="{active: index === currentIndex-1}"
+        <div  class="indi-item"
+              v-for="(item, index) in slideCount"
+             :class="{active: index === currentIndex-1}"
              :key="index"></div>
       </slot>
     </div>
